7f37eefcbf77a846cf13268ed8c21088f411b76d
terkper2000 <153760534+terkper2000@users.noreply.github.com>
2025-02-21 07:51:36 -0700

    Create Mod_2_unit_5_6.cs

---
+++ Mod_2_unit_5_6.cs
@@ -0,0 +1,5 @@
+//challenge code
+string name = "Hannah";
+int messageCount = 256;
+double temp = 1.5;
+Console.WriteLine($"Hello, {name}! You have {messageCount} messages in your inbox. The temperature is {temp}.");
